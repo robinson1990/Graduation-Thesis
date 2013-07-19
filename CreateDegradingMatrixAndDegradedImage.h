1a7f6b15545bfe3007b4eebb5f33bed7669a57f9
robinson1990 <n.wataru220@gmail.com>
2013-07-19 20:14:34 +0900

    bityousei

--- CreateDegradingMatrixAndDegradedImage.h
+++
@@ -1,4 +0,0 @@
-#ifndef INCLUDED_CREATEDEGRADINGMATRIXANDDEGRADEDIMAGE
-#define INCLUDED_CREATEDEGRADINGMATRIXANDDEGRADEDIMAGE
-cv::SparseMat CreateDegradingMatrixAndDegradedImage(cv::Mat&,cv::Mat&,cv::Point2d,int);
-#endif
